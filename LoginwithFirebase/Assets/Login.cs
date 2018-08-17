2ab0a4975325e404c417d50c18375e1bf422c2f8
Rameshkumar789 <rameshkumarkorlakunta@gmail.com>
2018-08-17 13:52:35 +0530

    First commit

---
+++ LoginwithFirebase/Assets/Login.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+using Facebook.Unity;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+
+	//public Text userIdText;
+	private FirebaseAuth mAuth;
+	
+
+	private void Awake()
+	{
+		
+	 Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+			
+		if (!FB.IsInitialized)
+		{
+			FB.Init();
+		}
+		else
+		{
+			FB.ActivateApp();
+            
+        }
+	}
+
+	public void LogIn()
+	{
+		FB.LogInWithReadPermissions(callback:OnLogIn);
+
+
+	}
+	private void OnLogIn(ILoginResult result)
+	{
+		if (FB.IsLoggedIn)
+		{
+			AccessToken tocken = AccessToken.CurrentAccessToken;
+			Debug.Log("login successful");
+			//userIdText.text = tocken.UserId;
+			Credential credential = FacebookAuthProvider.GetCredential(tocken.TokenString);
+			accessToken (credential);
+            SceneManager.LoadScene("AfterLogin");
+
+            Debug.Log("Change of Scence");
+
+        }
+		else
+		{
+			Debug.Log("Login Failed");
+		}
+	}
+
+	public void accessToken(Credential firebaseResult)
+	{
+		FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+		if (!FB.IsLoggedIn)
+		{
+			return;
+		}
+
+		auth.SignInWithCredentialAsync(firebaseResult).ContinueWith(task =>
+			{
+				if (task.IsCanceled)
+				{
+					Debug.LogError("SignInWithCredentialAsync was canceled.");
+					return;
+				}
+				if (task.IsFaulted)
+				{
+					Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+					return;
+				}
+
+				FirebaseUser newUser = task.Result;
+				Debug.LogFormat("User signed in successfully: {0} ({1})",
+					newUser.DisplayName, newUser.UserId);
+        });
+	}
+}
+
---
+++ LoginwithFirebase/Assets/UsageData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsageData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("usage Data");
+        //Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        //Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        //if (user != null)
+        //{
+        //    string name = user.DisplayName;
+        //    string email = user.Email;
+        //    System.Uri photo_url = user.PhotoUrl;
+        //    // The user's Id, unique to the Firebase project.
+        //    // Do NOT use this value to authenticate with your backend server, if you
+        //    // have one; use User.TokenAsync() instead.
+        //    string uid = user.UserId;
+        //    Debug.Log(name + "" + email + "" + uid);
+        //}
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
